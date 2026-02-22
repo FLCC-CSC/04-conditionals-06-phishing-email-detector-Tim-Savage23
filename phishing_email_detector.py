dc57f29c3f021742e7635395b0b42dc87b011579
Timothy Savage <tsavage4@fingerlakes.edu>
2026-02-22 15:34:22 -0500

    Update phishing_email_detector.py

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -14,7 +14,7 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
-subject = input("Enter the email subject line: ")
+
 subject = subject.lower()
 if ("urgent" in subject) or ("immediate action required" in subject):
     print("HIGH RISK: Possible phishing attempt.")
